29fee2443ee6aff27610f40aee0b5542636ea360
Daniel <dani.07rojas94@gmail.com>
2025-08-07 13:37:49 -0500

    Primera prueba despliegue v10

--- src/main/java/co/cleaniron/configuration/SpaRedirectConfig.java
+++ src/main/java/co/cleaniron/configuration/SpaRedirectConfig.java
@@ -1,15 +1,24 @@
 package co.cleaniron.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class SpaRedirectConfig implements WebMvcConfigurer {
 
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Sirve todo lo que esté en /static/**
+        registry
+                .addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Forward de “/” hacia index.html
+        // Forward de la raíz
         registry.addViewController("/").setViewName("forward:/index.html");
+        // Forward de cualquier ruta “amigable” a index.html (para rutas tipo /foo/bar)
+        registry.addViewController("/{path:[^\\.]*}").setViewName("forward:/index.html");
     }
 }
