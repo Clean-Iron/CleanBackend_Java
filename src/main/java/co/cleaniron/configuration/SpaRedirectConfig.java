7d76b01d74eee0efadd930282973a1953f77dac6
Daniel <dani.07rojas94@gmail.com>
2025-08-07 13:16:25 -0500

    Primera prueba despliegue v8

---
+++ src/main/java/co/cleaniron/configuration/SpaRedirectConfig.java
@@ -0,0 +1,15 @@
+package co.cleaniron.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpaRedirectConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward de “/” hacia index.html
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+}
